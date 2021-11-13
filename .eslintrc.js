b211364b7049296d8329e292430067cc1b2329f0
ktaicoder <ktaicodingblock.dev@gmail.com>
2021-11-13 12:13:06 +0900

    update3

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,18 +13,13 @@ module.exports = {
         sourceType: 'module',
     },
 
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:prettier/recommended',
-        'plugin:workspaces/recommended',
-    ],
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
     globals: {
         Atomics: 'readonly',
         SharedArrayBuffer: 'readonly',
     },
 
-    plugins: ['prettier', 'workspaces'],
+    plugins: ['prettier'],
     rules: {
         'no-useless-constructor': 'off',
         '@typescript-eslint/explicit-function-return-type': 'off',
