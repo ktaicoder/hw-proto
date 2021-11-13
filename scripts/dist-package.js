169b7392e663556059b408e0ccccea90147eb45b
ktaicoder <ktaicodingblock.dev@gmail.com>
2021-11-13 16:31:34 +0900

    release v1.0.8

--- scripts/dist-package.js
+++ scripts/dist-package.js
@@ -1,9 +1,24 @@
 const shell = require('shelljs')
+const fs = require('fs')
+
+function updatePackageDistJson() {
+    const fromPkg = JSON.parse(fs.readFileSync('package.json'))
+    const toPkg = JSON.parse(fs.readFileSync('package-dist.json'))
+
+    toPkg.version = fromPkg.version
+    toPkg.bugs = fromPkg.bugs
+    toPkg.homepage = fromPkg.homepage
+    toPkg.keywords = fromPkg.keywords
+    toPkg.description = fromPkg.description
+    toPkg.description = fromPkg.description
+    fs.writeFileSync('package-dist.json', JSON.stringify(toPkg, null, 4))
+}
 
 async function main() {
     shell.rm('-rf', 'dist')
     shell.mkdir('dist')
     shell.cp('-rf', './build/cjs', 'dist/')
+    updatePackageDistJson()
     shell.cp('-rf', 'package-dist.json', 'dist/package.json')
 }
 
